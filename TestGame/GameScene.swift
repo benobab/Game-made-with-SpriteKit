3adf97162ad407082d367976e61ac828dd92ad98
Ben <benlacroix83@gmail.com>
2016-02-22 17:00:24 +0100

    add gravity and physic body to the player

--- TestGame/GameScene.swift
+++ TestGame/GameScene.swift
@@ -16,6 +16,9 @@ class GameScene: SKScene {
     var foregroundNode: SKNode!
     var hudNode: SKNode!
     
+    // Player
+    var player: SKNode!
+    
     // To Accommodate iPhone 6
     var scaleFactor: CGFloat!
     
@@ -27,12 +30,23 @@ class GameScene: SKScene {
         super.init(size: size)
         backgroundColor = SKColor.whiteColor()
         
+        // Add some gravity
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
+        
         scaleFactor = self.size.width / 320.0
         
         // Create the game nodes
         // Background
         backgroundNode = createBackgroundNode()
         addChild(backgroundNode)
+        
+        // Foreground
+        foregroundNode = SKNode()
+        addChild(foregroundNode)
+        
+        // Add the player
+        player = createPlayer()
+        foregroundNode.addChild(player)
     }
     
     func createBackgroundNode() -> SKNode {
@@ -57,5 +71,29 @@ class GameScene: SKScene {
         // 6
         // Return the completed background node
         return backgroundNode
+    }
+    
+    func createPlayer() -> SKNode {
+        
+        let playerNode = SKNode()
+        
+        playerNode.position = CGPoint(x: self.size.width / 2, y: 80.0)
+        
+        let sprite = SKSpriteNode(imageNamed: "Player")
+        playerNode.addChild(sprite)
+        //ADD PHYSIC BODY TO BE ABLE TO PUT GRAVITY ETC
+        // 1
+        playerNode.physicsBody = SKPhysicsBody(circleOfRadius: sprite.size.width / 2)
+        // 2
+        playerNode.physicsBody?.dynamic = true
+        // 3
+        playerNode.physicsBody?.allowsRotation = false
+        // 4
+        playerNode.physicsBody?.restitution = 1.0
+        playerNode.physicsBody?.friction = 0.0
+        playerNode.physicsBody?.angularDamping = 0.0
+        playerNode.physicsBody?.linearDamping = 0.0
+        
+        return playerNode
     }
 }
